60166cdf63b2fb3d757618346c869f1fc5be7e9c
René Mettler <rmettler@anybotics.com>
2023-07-11 13:38:56 +0200

    change theme to catppuccin

--- init.lua
+++ init.lua
@@ -95,10 +95,11 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
