59aca2ed5cd24bf13b9e19170f4b5fe7b0df66e3
René Mettler <rene.mettler92@gmail.com>
2023-11-30 17:45:12 +0100

    migrate blanklines to v3

--- lua/plugins/blank_lines.lua
+++ lua/plugins/blank_lines.lua
@@ -2,6 +2,7 @@ return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
   -- See `:help indent_blankline.txt`
+  main = "ibl",
   lazy = false,
   opts = {
     char = '┊',
